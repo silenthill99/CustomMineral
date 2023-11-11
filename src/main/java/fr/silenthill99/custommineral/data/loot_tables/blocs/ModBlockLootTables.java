234df66e774e9b5105026e9557d75f7cad50ea47
silenthill99 <florian.graziani@sfr.fr>
2023-11-11 18:30:06 +0100

    Rajout de fichiers

--- src/main/java/fr/silenthill99/custommineral/data/loot_tables/blocs/ModBlockLootTables.java
+++ src/main/java/fr/silenthill99/custommineral/data/loot_tables/blocs/ModBlockLootTables.java
@@ -4,6 +4,11 @@ import fr.silenthill99.custommineral.init.ModBlocks;
 import fr.silenthill99.custommineral.init.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.data.loot.BlockLootTables;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.loot.ItemLootEntry;
+import net.minecraft.loot.RandomValueRange;
+import net.minecraft.loot.functions.ApplyBonus;
+import net.minecraft.loot.functions.SetCount;
 import net.minecraftforge.fml.RegistryObject;
 
 import java.util.stream.Collectors;
@@ -13,7 +18,7 @@ public class ModBlockLootTables extends BlockLootTables {
     protected void addTables() {
         this.dropSelf(ModBlocks.ULINITE_BLOCK.get());
         this.dropSelf(ModBlocks.ULINITE_ORE.get());
-        this.add(ModBlocks.AMETHYST_ORE.get(), (ore) -> createOreDrop(ore, ModItems.AMETHYST.get()));
+        this.add(ModBlocks.AMETHYST_ORE.get(), (ore) -> createSilkTouchDispatchTable(ore, applyExplosionDecay(ore, ItemLootEntry.lootTableItem(ModItems.AMETHYST.get()).apply(SetCount.setCount(RandomValueRange.between(2, 10))).apply(ApplyBonus.addUniformBonusCount(Enchantments.BLOCK_FORTUNE)))));
     }
 
     @SuppressWarnings("NullableProblems")
